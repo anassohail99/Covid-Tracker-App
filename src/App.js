76ef03abce4a109b11acc6e1fed56c5bcff61b0d
anassohail99 <anas.sohailabs1@gmail.com>
2020-06-19 02:31:50 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ class App extends Component {
     return (
       <div className={styles.container}>
         <Cards data={data} />
-        <CountryPicker />
-        <Chart />
+        {/* <CountryPicker />
+        <Chart /> */}
       </div>
     );
   }
