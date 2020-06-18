0504d0ef3f00a92e07051c3ff0777e1bb3ee4e95
anassohail99 <anas.sohailabs1@gmail.com>
2020-06-19 01:06:49 +0500

    Components Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
+import { Cards, Chart, CountryPicker } from "./Components";
+import styles from "./App.module.css";
+import { fetchData } from "./api/index";
 
 class App extends Component {
+  state = {
+    data: {},
+  };
+
+  async componentDidMount() {
+    const FetchData = await fetchData();
+    this.setState({ data: FetchData });
+  }
   render() {
+    const { data } = this.state;
     return (
-      <div>
-        <h1>App</h1>
+      <div className={styles.container}>
+        <Cards data={data} />
+        <CountryPicker />
+        <Chart />
       </div>
     );
   }
