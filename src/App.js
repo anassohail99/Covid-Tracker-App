e483c0b45cec43b3edd9571f055cde2c4ff45399
anassohail99 <anas.sohailabs1@gmail.com>
2020-06-20 01:00:53 +0500

    Updated

--- src/App.js
+++ src/App.js
@@ -6,19 +6,25 @@ import { fetchData } from "./api/index";
 class App extends Component {
   state = {
     data: {},
+    country: "",
   };
 
   async componentDidMount() {
     const FetchData = await fetchData();
     this.setState({ data: FetchData });
   }
+
+  handleCountryChange = async (country) => {};
+
   render() {
     const { data } = this.state;
     return (
       <div className={styles.container}>
         <Cards data={data} />
-        {/* <CountryPicker />
-        <Chart /> */}
+        <CountryPicker
+          handleCountryChange={this.componentWillUnmounthandleCountryChange}
+        />
+        <Chart />
       </div>
     );
   }
